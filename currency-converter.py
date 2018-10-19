7d53d04ab709c1718ec5fa6560bf1db701641616
qlem <clement.dommerc@gmail.com>
2018-10-19 18:08:19 +0100

    fix + comments + screenshots

--- currency-converter.py
+++ currency-converter.py
@@ -8,17 +8,19 @@ import zipfile
 import pyqtgraph as pg
 
 
+# Create, display and refresh UI
 class CurrencyConverter(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        # init var
+        # initialize variables and download / parse the data file
         self.data = {}
+        self.last_date = QDate().currentDate()
         self.currencies = []
         self.period = []
         self.download_unzip()
 
-        # init widget
+        # initialize widgets
         self.from_currency_label = QLabel("From currency:")
         self.from_currency = QComboBox()
         self.from_currency.addItems(self.currencies)
@@ -36,7 +38,7 @@ class CurrencyConverter(QDialog):
         self.rates_plot = pg.PlotWidget()
         self.legend = self.rates_plot.addLegend()
 
-        # set widget into layout
+        # set widgets into layout
         grid = QGridLayout()
         grid.addWidget(self.from_currency_label, 0, 0)
         grid.addWidget(self.from_currency, 0, 1)
@@ -52,19 +54,19 @@ class CurrencyConverter(QDialog):
         self.setLayout(grid)
         self.setWindowTitle("Currency Converter - Assignment 1 - Dommerc - 2982021")
 
-        # refresh ui
+        # refresh data
         today = QDate.currentDate()
         self.from_date.setSelectedDate(today.addDays(-10))
         self.update_ui()
 
-        # set event
+        # set event when input change (refresh ui)
         self.from_currency.currentIndexChanged.connect(self.update_ui)
         self.to_currency.currentIndexChanged.connect(self.update_ui)
         self.from_amount.valueChanged.connect(self.update_ui)
         self.from_date.selectionChanged.connect(self.update_ui)
         self.to_date.selectionChanged.connect(self.update_ui)
 
-    # method that return the 1st valid date
+    # Method that return the 1st valid date by subtraction or addition one day
     def get_valid_date(self, date, sign):
         tmp = date
         while tmp not in self.data["USD"]:
@@ -74,27 +76,27 @@ class CurrencyConverter(QDialog):
                 tmp = tmp.addDays(-1)
         return tmp
 
-    # method that return
-    def set_period(self, from_, to):
-        tmp = from_
-        to = self.get_valid_date(to, '-')
+    # Method that set the period according to the two selected dates
+    def set_period(self, from_date, to_date):
+        tmp = from_date
+        to_date = self.get_valid_date(to_date, '-')
         self.period.clear()
-        while tmp <= to:
+        while tmp <= to_date:
             tmp = self.get_valid_date(tmp, '+')
-            if tmp <= to:
+            if tmp <= to_date:
                 self.period.append(tmp)
             tmp = tmp.addDays(+1)
 
-    # method that return
+    # Method that return rates according to the period and a currency
     def get_rates(self, currency):
         rates = []
         for date in self.period:
             rates.append(self.data[currency][date])
         return rates
 
-    # method that return
+    # Method that return conversion rates according to the two selected currencies
     @staticmethod
-    def get_conversion_rate(rates_from, rates_to):
+    def get_conversion_rates(rates_from, rates_to):
         rates_cv = []
         i = 0
         while i < len(rates_from):
@@ -104,12 +106,17 @@ class CurrencyConverter(QDialog):
             i += 1
         return rates_cv
 
+    # Refresh all data displayed in the window
     def update_ui(self):
         try:
-            # refresh ui
+            # set maximum and minimum date for the two date picker
+            self.to_date.setMaximumDate(QDate.currentDate())
+            self.to_date.setMinimumDate(self.from_date.selectedDate())
+            self.from_date.setMaximumDate(self.to_date.selectedDate())
+            self.from_date.setMinimumDate(self.last_date)
+
+            # compute the conversion value
             date = self.get_valid_date(QDate.currentDate(), '-')
-            from_date = self.from_date.selectedDate()
-            to_date = self.to_date.selectedDate()
             from_cur = self.from_currency.currentText()
             to_cur = self.to_currency.currentText()
             from_rate = self.data[from_cur][date]
@@ -119,16 +126,17 @@ class CurrencyConverter(QDialog):
                 res = (to_rate / from_rate) * amount
                 self.to_amount.setText("%.02f" % res)
             else:
-                self.to_amount.setText("NaN")
+                self.to_amount.setText("no data")
 
-            # refresh graph
+            # set period and get rates
+            from_date = self.from_date.selectedDate()
+            to_date = self.to_date.selectedDate()
             self.set_period(from_date, to_date)
             rates_from = self.get_rates(from_cur)
             rates_to = self.get_rates(to_cur)
-            rates_cv = self.get_conversion_rate(rates_from, rates_to)
+            rates_cv = self.get_conversion_rates(rates_from, rates_to)
 
             # debug
-            '''
             print("\n")
             print(self.period)
             print("\n")
@@ -138,12 +146,16 @@ class CurrencyConverter(QDialog):
             print("\n")
             print(rates_cv)
             print("\n")
-            '''
 
+            # clear plots and legend
             self.rates_plot.clear()
             self.legend.scene().removeItem(self.legend)
             self.legend = self.rates_plot.addLegend()
 
+            if len(self.period) == 0:
+                return
+
+            # set labels and ranges
             self.rates_plot.setLabel('left', 'Rate')
             self.rates_plot.setLabel('bottom', 'Days')
             date_range = range(0, len(self.period))
@@ -156,6 +168,7 @@ class CurrencyConverter(QDialog):
                 max_ = max(max(rates_from), max(rates_to))
             self.rates_plot.setYRange(min_, max_)
 
+            # set plots
             self.rates_plot.plot(date_range, rates_from, pen='b', symbol='o', symbolPen='b', symbolBrush=0.2,
                                  name=from_cur)
             self.rates_plot.plot(date_range, rates_to, pen='g', symbol='x', symbolPen='g', symbolBrush=0.2, name=to_cur)
@@ -166,25 +179,36 @@ class CurrencyConverter(QDialog):
         except Exception as e:
             print(e)
 
+    # Method that download data file and proceed to parsing
     def download_unzip(self):
-        url = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip'
-        self.file, _ = urlretrieve(url)
-        zip_file_object = zipfile.ZipFile(self.file, 'r')
-        first_file = zip_file_object.namelist()[0]
-        self.file = zip_file_object.open(first_file)
-        content = self.file.read()
 
-        # parsing file
+        # download the file
+        url = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip'
+        raw, _ = urlretrieve(url)
+        zip_file_object = zipfile.ZipFile(raw, 'r')
+        first_file = zip_file_object.namelist()[0]
+        file = zip_file_object.open(first_file)
+        content = file.read()
+
+        # parsing the file
         lines = content.decode().split("\n")
         print(len(lines))
+
+        # get the last date of the file
+        last_line = lines[len(lines) - 1]
+        array = last_line.split(",")
+        array = array[0].split("-")
+        self.last_date = QDate(int(array[0]), int(array[1]), int(array[2]))
+
+        # get all available currencies
         self.currencies = lines[0].split(",")
         self.currencies.pop(0)
         self.currencies.pop(len(self.currencies) - 1)
-
         for cur in self.currencies:
             self.data[cur] = {}
         lines.pop(0)
 
+        # get value of each currency for each date
         for line in lines:
             items = line.split(",")
             array = items[0].split("-")
@@ -198,6 +222,7 @@ class CurrencyConverter(QDialog):
                     self.data[self.currencies[idx]][date] = float(item)
 
 
+# Main function, it is the entry point.
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     currency_converter = CurrencyConverter()
